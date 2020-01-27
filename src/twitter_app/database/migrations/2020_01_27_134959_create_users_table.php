24df91bb102e605daf5327e39d375a41b13d03e3
miko.fugo <okuramarusaikou@outlook.jp>
2020-01-27 23:03:34 +0900

    refs #1 ユーザーテーブル追加

---
+++ src/twitter_app/database/migrations/2020_01_27_134959_create_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('user_id');
+            $table->string('user_name')->nullable(false);
+            $table->string('user_password')->nullable(false);
+            $table->timestamps('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
