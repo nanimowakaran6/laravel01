6b0f51fba9dbb046853fefeee3fe21564931e3fa
miko.fugo <okuramarusaikou@outlook.jp>
2020-02-02 15:55:53 +0900

    refs #1 ディレクトリ変更

--- src/database/migrations/2020_01_27_134959_create_users_table.php
+++ src/database/migrations/2020_01_27_134959_create_users_table.php
@@ -14,12 +14,10 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->increments('user_id');
+            $table->string('user_name')->nullable(false);
+            $table->string('user_password')->nullable(false);
+            $table->timestamps('created_at');
         });
     }
 
