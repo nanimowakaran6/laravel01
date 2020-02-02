4d1f577570f44c55d7e2c9f534e1899c817d5be4
miko.fugo <okuramarusaikou@outlook.jp>
2020-02-02 23:29:48 +0900

    refs #1 必要なカラムを削除してしまっていたので修正

--- src/database/migrations/2020_01_27_140555_create_twitter_accounts_table.php
+++ src/database/migrations/2020_01_27_140555_create_twitter_accounts_table.php
@@ -16,6 +16,7 @@ class CreateTwitterAccountsTable extends Migration
         Schema::create('twitter_accounts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('twitter_account');
+            $table->integer('user_id')->unsigned();
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users');
         });
--- src/database/migrations/2020_01_27_140613_create_twitter_words_table.php
+++ src/database/migrations/2020_01_27_140613_create_twitter_words_table.php
@@ -16,6 +16,7 @@ class CreateTwitterWordsTable extends Migration
         Schema::create('twitter_words', function (Blueprint $table) {
             $table->increments('id');
             $table->string('twitter_word');
+            $table->integer('user_id')->unsigned();
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users');
         });
